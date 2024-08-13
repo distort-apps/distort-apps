655d4d85b775e97a513f2be5eb93b324aab3f51a
Daniel <danoladesign@gmail.com>
2024-08-13 16:59:24 -0400

    DSA001: deleted some things temp to build for vercel.

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,56 +1,59 @@
-// import Image from "next/image";
-// import { notFound } from "next/navigation";
+// import { getProject } from "@/lib/projects";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import Video from "@/components/video/video";
 
-// interface ProjectDetailParams {
-//   params: {
-//     slug: string;
-//   };
-// }
-// const isVideo = (media: string): boolean => {
-//   const videoIndicators = ["video", ".mp4", ".webm", ".ogg"];
-//   return videoIndicators.some((indicator) => media.includes(indicator));
-// };
+interface ProjectDetailParams {
+  params: {
+    slug: string;
+  };
+}
+const isVideo = (media: string): boolean => {
+  const videoIndicators = ["video", ".mp4", ".webm", ".ogg"];
+  return videoIndicators.some((indicator) => media.includes(indicator));
+};
 
-// export default async function ArticleDetailPage({ params }: ArticleDetailParams) {
+export default async function ProjectDetailPage({ params }: ProjectDetailParams) {
   
-//   const article = await getArticle(params.slug);
-//   const mediaIsVideo = isVideo(article.media);
-//   const humanReadableDate = new Date(article.date).toLocaleDateString('en-US', {
+//   const project = await getProject(params.slug);
+//   const mediaIsVideo = isVideo(project.media[1]);
+//   const humanReadableDate = new Date(project.date).toLocaleDateString('en-US', {
 //     month: 'long',
 //     day: 'numeric',
 //     year: 'numeric'
 //   })
-//   if (!article) {
+//   if (!project) {
 //     notFound();
 //   }
   
-//   article.body = article.body.replace(/\n/g,'<br />')
+//   project.body = project.body.replace(/\n/g,'<br />')
 
 
-//   return (
-//     <>
-//       <header className={classes.header}>
-//         <div className={classes.headerText}>
-//         <h1>{article.headline}</h1>
-//       <div className={classes.image}>
-//           {mediaIsVideo ? (
-//             <Video media={article.media} />
-//             ) : (
-//               <Image src={article.media} alt={article.slug} fill />
-//               )}
-//         </div>
-//           <p>{article.location}</p>
-//           <p>{humanReadableDate}</p>
-//         </div>
-//         <main>
-//         <p
-//           className={classes.instructions}
-//           dangerouslySetInnerHTML={{
-//             __html: article.body,
-//           }}
-//         ></p>
-//       </main>
-//       </header>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <header className={classes.header}>
+        {/* <div className={classes.headerText}>
+        <h1>{project.title}</h1>
+      <div className={classes.image}>
+          {mediaIsVideo ? (
+            <Video media={project.media[1]} />
+            ) : (
+              <Image src={project.media[1]} alt={project.slug} fill />
+              )}
+        </div>
+          <p>{project.body}</p>
+          <p>{humanReadableDate}</p>
+        </div>
+        <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: project.body,
+          }}
+        ></p>
+      </main> */}
+      </header>
+    </>
+  );
+}
